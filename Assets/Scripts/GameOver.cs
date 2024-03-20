1dde3cf9cc1c90b57ac6ecbdab12eaf36b786935
Ishmael Kwayisi <spyrorocks324@gmail.com>
2024-03-19 20:57:45 -0400

    3 New Scenes
    In this commit, I have a title screen, game over screen and an ending screen(with animation).

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public void tryAgain()
+    {
+        SceneManager.LoadScene("dungeon1_scene1");
+    }
+}
