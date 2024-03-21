652107af4938f798278f578f5f0520c365b88b59
Austin Hoang <austinqhoang245@gmail.com>
2024-03-21 16:16:53 -0400

    Bow Animations + Optimizations
    -Made bow animations
    -Threw all animations scattered in the sprites folder into animations folder
    
    -Fuck unity and github for being a pain in the ass holy fucking shit, changing a script broke the entire fucking namespace somehow great fucking job unity, imma kill myself now

--- Assets/Scripts/AttackArea.cs
+++ Assets/Scripts/AttackArea.cs
@@ -2,18 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public MonsterDamage nib;
 public class AttackArea : MonoBehaviour
 
 {
-    private int damage = 2;
-
-    private void OnTriggerEnter2D(Collider2D collider)
-    {
-      if(collider.GetComponent<health>() !- null)
-        {
-            health health = collider.GetComponent<health>();
-        }
-    }
+  
 
 }
