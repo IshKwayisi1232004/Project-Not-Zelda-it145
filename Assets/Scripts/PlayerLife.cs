e9deea457e11a368cb9021923b753ca1b98aa685
Ishmael Kwayisi <spyrorocks324@gmail.com>
2024-03-21 11:27:17 -0400

    Player Can Collect Items Now

--- Assets/Scripts/PlayerLife.cs
+++ Assets/Scripts/PlayerLife.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerLife : MonoBehaviour
 {
@@ -22,6 +23,7 @@ public class PlayerLife : MonoBehaviour
         {
             anim.SetBool("IsDead", true);
             Destroy(gameObject);
+            SceneManager.LoadScene("GameOver");
         }
     }
 
