5822716b3cb6ac325214e396643916bdc66f9f7a
IsaacAkh <mohammadasahaq.akhtarzada@snhu.edu>
2024-04-03 13:53:47 -0400

    Projectile
    Pl

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float initialSpeed = 10f;
+    public float angle = 45f;
+    private Rigidbody rb;
+    private Vector3 initialVelocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        CalculateInitialVelocity();
+        LaunchProjectile();
+    }
+
+    void CalculateInitialVelocity()
+    {
+        float radianAngle = angle * Mathf.Deg2Rad;
+        float xVelocity = initialSpeed * Mathf.Cos(radianAngle);
+        float yVelocity = initialSpeed * Mathf.Sin(radianAngle);
+        initialVelocity = new Vector3(xVelocity, yVelocity, 0f);
+    }
+
+    void LaunchProjectile()
+    {
+        rb.velocity = initialVelocity;
+    }
+}
